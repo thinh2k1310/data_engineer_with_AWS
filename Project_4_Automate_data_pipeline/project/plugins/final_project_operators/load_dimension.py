5d9fab22237b56d736ad49983ce2037dbdee0162
Trương Công Quốc Thịnh <75033729+thinh2k1310@users.noreply.github.com>
2023-07-31 09:23:03 +0700

    Allows to switch between append and truncate-insert at Load dimension operator

--- Project_4_Automate_data_pipeline/project/plugins/final_project_operators/load_dimension.py
+++ Project_4_Automate_data_pipeline/project/plugins/final_project_operators/load_dimension.py
@@ -10,13 +10,17 @@ class LoadDimensionOperator(BaseOperator):
     def __init__(self,
                  redshift_conn_id="",
                  sql_query="",
+                 truncate="",
                  *args, **kwargs):
 
         super(LoadDimensionOperator, self).__init__(*args, **kwargs)
         self.redshift_conn_id = redshift_conn_id
         self.sql_query = sql_query
+        self.truncate = truncate
 
     def execute(self, context):
         redshift = PostgresHook(postgres_conn_id=self.redshift_conn_id)
+        if self.truncate:
+            redshift.run(f"TRUNCATE TABLE {self.table}")
         self.log.info("Load data from staging to dimension table")
         redshift.run(self.sql_query)
